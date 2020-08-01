20af45a460f9ced7592b9d81d43729f8369bff08
Caleb Stripes <calebdstripes@gmail.com>
2020-07-31 20:22:03 -0400

    did some fun stuff with system dates and user inputs

--- Program.cs
+++ Program.cs
@@ -8,21 +8,29 @@ namespace c_sharp_hello_world
     {
         static void Main(string[] args)
         {
+            // Find out the users name.
             Console.WriteLine("What is your name?");
-            string input = Console.ReadLine();
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("My name is " + input + "!");
-
             Console.WriteLine();
+
+            string input = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello World! My name is " + input + "!");
+
+            // Find out when the user started coding.
             Console.WriteLine("What year did you start developing software?");
             string startYearString = Console.ReadLine();
-            _ = Convert.ToInt32(startYearString);
+            Console.WriteLine();
+            int startYearInt = Convert.ToInt32(startYearString);
 
-            Console.WriteLine(startYearString);
 
-            //string date = DateTime.UtcNow.ToString(MM-dd-yyyy);
-
-            Console.WriteLine("using a number in angle braces I can call an item in the following array. ie the third item using 2 would be 3:{2}", 1, 2, 3, 4);
+            // Determine how long the user has been coding in years.
+            DateTime localDate = DateTime.Now;
+            int localDateYear = localDate.Year;
+            int yearsCoding = localDateYear - startYearInt;
+            Console.WriteLine("You have been coding for " + yearsCoding + " years.");
+            
+            // an extra lession I learned trying to do what I wanted with the date.
+            // Console.WriteLine("using a number in angle braces I can call an item in the following array. ie the third item using 2 would be 3:{2}", 1, 2, 3, 4);
 
         }
     }
